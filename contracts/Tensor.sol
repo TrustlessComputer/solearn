8c9c8a95d9df4fde5e9a1e283228fa963075b607
1605 <rein1605@generative.xyz>
2023-04-18 18:20:11 +0700

    (WIP) Fix tensorflow code

--- contracts/Tensor.sol
+++ contracts/Tensor.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.9;
 
 import "solmate/src/utils/FixedPointMathLib.sol";
+import { SD59x18, sd, unwrap, wrap, abs, avg, mul, div, sqrt, powu, pow, exp, log2} from "@prb/math/src/SD59x18.sol";
 
 contract Tensor {
+	SD59x18 constant ONE = sd(1e18);
+	SD59x18 constant TWO = sd(2e18);
+	SD59x18 constant E = sd(2.718281828459045235e18);
+
 	struct TensorData {
-		uint[][] mat;
+		SD59x18[][] mat;
 		uint n;
 		uint m;
 	}
 
-	function flat(uint[][] memory mat) internal pure returns (uint[] memory) {
-		uint[] memory result = new uint[](mat.length * mat[0].length);
+	function flat(SD59x18[][] memory mat) internal pure returns (SD59x18[] memory) {
+		SD59x18[] memory result = new SD59x18[](mat.length * mat[0].length);
 		uint p = 0;
 		for (uint i = 0; i < mat.length; i++) {
 			for (uint j = 0; j < mat[i].length; j++) {
@@ -23,14 +28,14 @@ contract Tensor {
 	}
 
 
-	function loadm(TensorData memory ts, uint[] memory data, uint n, uint m) public {
+	function loadm(TensorData memory ts, SD59x18[] memory data, uint n, uint m) public {
 		ts.n = n;
 		ts.m = m;
-		ts.mat = new uint[][](n);
+		ts.mat = new SD59x18[][](n);
 
 		uint p = 0;
 		for (uint i = 0; i < n; i++) {
-			ts.mat[i] = new uint[](m);
+			ts.mat[i] = new SD59x18[](m);
 			for (uint j = 0; j < m; j++) {
 				ts.mat[i][j] = p < data.length ? data[p] : 0;
 				p += 1;
@@ -42,7 +47,7 @@ contract Tensor {
 	// function load(TensorData memory ts, uint[] memory data, uint n, uint m) public {
 	// 	ts.n = n;
 	// 	ts.m = m;
-	// 	ts.mat = new uint[][](n);
+	// 	ts.mat = new SD59x18[][](n);
 
 	// 	uint p = 0;
 	// 	for (uint i = 0; i < n; i++) {
@@ -60,29 +65,30 @@ contract Tensor {
 		return result;
 	}
 
-	function __linear(uint x) internal pure returns (uint) {
+	function __linear(SD59x18 x) internal pure returns (SD59x18) {
 		return x;
 	}
 
-	function __relu(uint x) internal pure returns (uint) {
+	function __relu(SD59x18 x) internal pure returns (SD59x18) {
 		return x > 0 ? x : 0;
 	}
 
-	function __leaky_relu(uint x) internal pure returns (uint) {
-		return x > 0 ? x : x * 2 / 10;
+	function __leaky_relu(SD59x18 x) internal pure returns (SD59x18) {
+		return x > 0 ? x : x * sd(0.2e18);
 	}
 
-	function __sigmoid(uint x) internal pure returns (uint) {
-		return 1 / (1 + 2**(-x));
+	function __sigmoid(SD59x18 x) internal pure returns (SD59x18) {
+		return ONE / (ONE + pow(E, -x));
 	}
 
-	function __tanh(uint x) internal pure returns (uint) {
-		return (2 / (1 + 2**(-2 * x))) - 1;
+	function __tanh(SD59x18 x) internal pure returns (SD59x18) {
+		SD59x18 t = pow(E, TWO * x);
+		return (t - 1)/(t + 1);
 	}
 
 	function __apply_unary_op(
 		TensorData memory a,
-		function(uint) internal pure returns (uint) op
+		function(SD59x18) internal pure returns (SD59x18) op
 	) internal pure returns (TensorData memory) {
 		TensorData memory res = cloneTensor(a);
 		for (uint i = 0; i < res.n; i++) {
@@ -113,23 +119,23 @@ contract Tensor {
 		return __apply_unary_op(a, __tanh);
 	}
 
-	function __add(uint a, uint b) internal pure returns (uint) {
+	function __add(SD59x18 a, SD59x18 b) internal pure returns (SD59x18) {
 		return a + b;
 	}
 
-	function __mul(uint a, uint b) internal pure returns (uint) {
+	function __mul(SD59x18 a, SD59x18 b) internal pure returns (SD59x18) {
 		return a * b;
 	}
 
-	function __exp(uint a, uint b) internal pure returns (uint) {
-		return FixedPointMathLib.rpow(a, b, FixedPointMathLib.WAD);
+	function __exp(SD59x18 a, SD59x18 b) internal pure returns (SD59x18) {
+		return pow(a, b);
 	}
 
     function __apply_binary_op(TensorData memory a, TensorData memory b, function(uint, uint) internal pure returns (uint) op) internal pure returns (uint) {
 		if (b.n == 0) {
 			b.n = 1;
 			b.m = 1;
-			b.mat = new uint[][](1);
+			b.mat = new SD59x18[][](1);
 		}
 
 		TensorData memory res = cloneTensor(a);
@@ -163,7 +169,7 @@ contract Tensor {
 
     function softmax(TensorData memory a) public pure returns (TensorData memory) {
 		TensorData memory res = __apply_unary_op(a, __exp);
-		uint sum_e = 0;
+		SD59x18 sum_e = 0;
 		for (uint i = 0; i < res.n; i++) {
 			for (uint j = 0; j < res.m; j++) {
 				sum_e += res.mat[i][j];
