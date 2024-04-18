14a1ec26317340f0a0b3b1183cc408d7e75946a1
rein-nbc <rein1605@newbitcoincity.com>
2024-04-18 15:26:45 +0700

    Add get pending request

--- scripts/base.task.ts
+++ scripts/base.task.ts
@@ -688,3 +688,13 @@ task("check-models")
 
         fs.writeFileSync("model_list_2.json", JSON.stringify(models));
     })
+
+task("get-pending-max-multi")
+    .addOptionalParam("workerhub", "worker_hub contract address", "0x5E077E883b9b44CC5213140c87d98DAB35E6390e", types.string)
+    .addOptionalParam("model", "model contract address", "0x0964Eb14A7DF17f1B5b56c43f2d9B5b8873d573C", types.string)
+    .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
+        const { ethers } = hre;
+        const c = await ethers.getContractAt('WorkerHub', taskArgs.workerhub);
+        const pendingRequests = await c.getModelUnresolvedInferences(taskArgs.model);
+        console.log(pendingRequests.length);
+    });
