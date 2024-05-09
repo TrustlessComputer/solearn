03f2eef9eec86909e1c4db05cca0eec871d69120
kochou-nbc <kochou@newbitcoincity.com>
2024-05-09 14:37:36 +0700

    update deploy scripts

--- scripts/deployHybridModel.ts
+++ scripts/deployHybridModel.ts
@@ -8,9 +8,17 @@ async function deployHybridModel() {
 
     const workerHubAddress = config.workerHubAddress;
     const identifier = 0;
-    const name = 'Futuristic Fragment';//`Model ${identifier}`;
-    const url = '';//`abc.com/${identifier}`;
-    const inferenceCost = ethers.utils.parseEther('100');
+    const name = 'Max Multi';//`Model ${identifier}`;
+    const metadata = '{\n' +
+        '\t"version": 1,\n' +
+        '\t"model_name": "MaxMulti",\n' +
+        '\t"model_type": "image",\n' +
+        '\t"model_url": "https://gateway.lighthouse.storage/ipfs/QmcFYMYpVodkpT6t1fVmWNjPnUnnQbXvwpqyheXvPGKUr8",\n' +
+        '\t"model_file_hash": "7f1f29cb884c5b2f4d072b99afcb87f32cbe4adc88cffedab15ffc9fd30887ae",\n' +
+        '\t"min_hardware": 1,\n' +
+        '\t"verifier_url": "https://gateway.lighthouse.storage/ipfs/QmdkKEjx2fauzbPh1j5bUiQXrUG5Ft36pJGHS8awrN89Dc",\n' +
+        '\t"verifier_file_hash": "492b2b3dea3003d359fe1b2cee77a22777d8a9faf942ab6dee81e6bfadaadec4"\n' +
+        '}'
 
     const hybridModel = await upgrades.deployProxy(
         HybridModel,
@@ -18,14 +26,13 @@ async function deployHybridModel() {
             workerHubAddress,
             identifier,
             name,
-            url,
-            inferenceCost
+            metadata
         ]
     );
     await hybridModel.deployed();
 
     const workerHub = WorkerHub.attach(workerHubAddress);
-    await workerHub.registerModel(hybridModel.address);
+    await workerHub.registerModel(hybridModel.address, 1, ethers.utils.parseEther('100'));
 
     console.log(`Contract HybridModel has been deployed to address ${hybridModel.address}`);
 
