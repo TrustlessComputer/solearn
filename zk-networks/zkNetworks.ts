c21603cd1b9e9280e622cafecb9e50c96fd1515f
reinSPQR <rein1605@bvm.network>
2024-09-24 14:04:12 +0700

    Minor fix

--- zk-networks/zkNetworks.ts
+++ zk-networks/zkNetworks.ts
@@ -3,6 +3,9 @@ export const zk_mainnets = {
     url: "https://rpc.testnet.supersonic2.bvm.network",
     chainId: 22219,
     accounts: [process.env.ZK_TESTNET_PRIVATE_KEY],
+    treasuryAddress: process.env.ZK_TESTNET_405B_TREASURY_ADDRESS,
+    collectionAddress: process.env.ZK_TESTNET_405B_COLLECTION_ADDRESS,
+    workerHubAddress: process.env.ZK_TESTNET_405B_WORKER_HUB_ADDRESS,
     allowUnlimitedContractSize: true,
     ethNetwork: "https://testnet.runechain.com/rpc", // The Ethereum Web3 RPC URL.
     zksync: true,
