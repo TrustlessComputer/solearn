71fa54e2ec1a24703828de6dae4ee6c4c11460df
kelvin2608 <kelvin@bvm.network>
2024-10-21 17:49:53 +0700

    [Flux] update duration param
    (cherry picked from commit 1a01f15131750c27b91c7160c00989946841eb29)

--- scripts/workerHubUpdatePenaltyParams.ts
+++ scripts/workerHubUpdatePenaltyParams.ts
@@ -8,25 +8,31 @@ async function getWorkerHubContract(proxyAddress: string) {
 
   return workerHub as WorkerHub;
 }
-async function workerHubUpdatePenaltyParams() {
+async function workerHubUpdateParamsx() {
   const config = network.config as any;
   const workerhub = await getWorkerHubContract(config.workerHubAddress);
   console.log("Workerhub address: ", config.workerHubAddress);
 
-  console.log("WorkerHub set penalty duration...");
-  const tx1 = await workerhub.setPenaltyDuration(0);
+  console.log("WorkerHub set submit duration...");
+  const tx1 = await workerhub.setSubmitDuration(1500);
   const receipt1 = await tx1.wait();
   console.log("Transaction hash: ", receipt1?.hash);
   console.log("Transaction status: ", receipt1?.status);
 
-  console.log("WorkerHub set penalty percentage...");
-  const tx2 = await workerhub.setFinePercentage(0);
+  console.log("WorkerHub set commit duration...");
+  const tx2 = await workerhub.setCommitDuration(1500);
   const receipt2 = await tx2.wait();
   console.log("Transaction hash: ", receipt2?.hash);
   console.log("Transaction status: ", receipt2?.status);
+
+  console.log("WorkerHub set reveal duration...");
+  const tx3 = await workerhub.setRevealDuration(1500);
+  const receipt3 = await tx3.wait();
+  console.log("Transaction hash: ", receipt3?.hash);
+  console.log("Transaction status: ", receipt3?.status);
 }
 
-workerHubUpdatePenaltyParams()
+workerHubUpdateParamsx()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
