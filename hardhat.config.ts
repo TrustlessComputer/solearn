34ee12d9b6d239b6b22cf2ca815984aceadfec94
kelvin2608 <kelvin@bvm.network>
2024-11-27 11:24:19 +0700

    Update Hardhat configuration for Polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,7 @@ import "./scripts/tasks/control.task";
 let localTestMnemonic =
   "test test test test test test test test test test test junk";
 const config: HardhatUserConfig = {
-  defaultNetwork: "zksync_mainnet",
+  defaultNetwork: "polygon_mainnet",
   solidity: {
     compilers: [
       {
@@ -402,6 +402,37 @@ const config: HardhatUserConfig = {
         process.env.ARBITRUM_MAINNET_SYSTEM_PROMPT_MANAGER_ADDRESS,
       squadManagerAddress: process.env.ARBITRUM_MAINNET_SQUAD_MANAGER_ADDRESS,
       wEAIAddress: process.env.ARBITRUM_MAINNET_WEAI,
+      allowUnlimitedContractSize: true,
+      ethNetwork: "https://testnet.runechain.com/rpc", // The Ethereum Web3 RPC URL.
+      zksync: false,
+      gasPrice: "auto",
+    } as any,
+    polygon_mainnet: {
+      url:
+        "https://polygon-mainnet.infura.io/v3" +
+        process.env.POLYGON_MAINNET_INFURA_API_KEY,
+      chainId: 137,
+      accounts: [
+        process.env.POLYGON_MAINNET_PRIVATE_KEY,
+        process.env.POLYGON_MAINNET_PRIVATE_KEY_WORKER_1,
+        process.env.POLYGON_MAINNET_PRIVATE_KEY_WORKER_2,
+        process.env.POLYGON_MAINNET_PRIVATE_KEY_WORKER_3,
+      ],
+      l2OwnerAddress: process.env.POLYGON_MAINNET_L2_OWNER_ADDRESS,
+      treasuryAddress: process.env.POLYGON_MAINNET_TREASURY_ADDRESS,
+      collectionAddress: process.env.POLYGON_MAINNET_COLLECTION_ADDRESS,
+      workerHubAddress: process.env.POLYGON_MAINNET_WORKER_HUB_ADDRESS,
+      stakingHubAddress: process.env.POLYGON_MAINNET_STAKING_HUB_ADDRESS,
+      daoTokenAddress: process.env.POLYGON_MAINNET_DAO_TOKEN_ADDRESS,
+      workerHubScoringAddress:
+        process.env.POLYGON_MAINNET_WORKER_HUB_SCORING_ADDRESS,
+      hybridModelAddress: process.env.POLYGON_MAINNET_HYBRID_MODEL_ADDRESS,
+      hybridModelScoringAddress:
+        process.env.POLYGON_MAINNET_HYBRID_MODEL_SCORING_ADDRESS,
+      systemPromptManagerAddress:
+        process.env.POLYGON_MAINNET_SYSTEM_PROMPT_MANAGER_ADDRESS,
+      squadManagerAddress: process.env.POLYGON_MAINNET_SQUAD_MANAGER_ADDRESS,
+      wEAIAddress: process.env.POLYGON_MAINNET_WEAI,
       allowUnlimitedContractSize: true,
       ethNetwork: "https://testnet.runechain.com/rpc", // The Ethereum Web3 RPC URL.
       zksync: false,
