98d11fe11e4cd644e74af6fc7cfc99b8ebc87a91
kelvin6868 <kelvin@bvm.network>
2024-12-24 16:59:19 +0700

    [BASE] Deploy new ModelLoadBalancer contract

--- scripts/initSubnet.ts
+++ scripts/initSubnet.ts
@@ -22,7 +22,7 @@ export async function getContractInstance(
   return contractIns;
 }
 
-async function main() {
+(async function main() {
   assert.ok(
     config.modelLoadBalancer,
     "ModelLoadBalancer address not found in config"
@@ -34,30 +34,42 @@ async function main() {
   )) as ModelLoadBalancer;
 
   // create group
-  const grName = "subnet1";
-  const listModelAddresses: string[] = [];
+  // const grName = "NousResearch/Hermes-3-Llama-3.1-70B-FP8";
+  // const grName = "neuralmagic/Meta-Llama-3.1-405B-Instruct-quantized.w4a16";
+  const grName = "lmstudio-community/INTELLECT-1-Instruct-GGUF";
+
+  // const listModelAddresses: string[] = [
+  //   "0x643c45e89769a16bcb870092bd1efe4696cb2ce7",
+  // ];
+  // const listModelAddresses: string[] = [
+  //   "0x9a35863baf0df7623f34fcf3376cdbac8581e76b",
+  // ];
+  const listModelAddresses: string[] = [
+    "0xF5A162E8169CAB83478283B4e36F50b80C0b33F2",
+    "0x1A9C75eF8E85A5D4E98B1E35239Ba98cfad1ee93",
+  ];
   const tx = await ins.createGroup(grName, listModelAddresses);
   const receipt = await tx.wait();
   console.log("Group created: ", grName, " tx hash: ", receipt?.hash);
-  console.log("Subnet of group: ", await ins.getSubnetsOfGroup(grName));
-}
-
-(async function getMetadata() {
-  const models = [
-    config.hybridModelAddress_1,
-    config.hybridModelAddress_2,
-    config.hybridModelAddress_3,
-    config.hybridModelAddress_4,
-  ];
-
-  for await (let model of models) {
-    const ins = (await getContractInstance(
-      model,
-      "HybridModel"
-    )) as HybridModel;
-
-    // get metadata
-    const metadata = await ins.metadata();
-    console.log(`Model ${model} - ${metadata}: \n\n`);
-  }
+  console.log("Cluster of group: ", await ins.getClustersOfGroup(grName));
 })();
+
+// (async function getMetadata() {
+//   const models = [
+//     config.hybridModelAddress_1,
+//     config.hybridModelAddress_2,
+//     config.hybridModelAddress_3,
+//     config.hybridModelAddress_4,
+//   ];
+
+//   for await (let model of models) {
+//     const ins = (await getContractInstance(
+//       model,
+//       "HybridModel"
+//     )) as HybridModel;
+
+//     // get metadata
+//     const metadata = await ins.metadata();
+//     console.log(`Model ${model} - ${metadata}: \n\n`);
+//   }
+// })();
