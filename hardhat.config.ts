dce132fb637b512f4aa59782e209a8884470d0fb
kelvin6868 <kelvin@bvm.network>
2025-02-17 14:34:26 +0700

    Prepare to deploy model DeepHermes to Base network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -217,6 +217,9 @@ const config: HardhatUserConfig = {
         process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_HERMES_1,
         process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_HERMES_2,
         process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_HERMES_3,
+        process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_DEEP_HERMES_1,
+        process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_DEEP_HERMES_2,
+        process.env.BASE_MAINNET_PRIVATE_KEY_WORKER_DEEP_HERMES_3,
       ],
       l2OwnerAddress: process.env.BASE_MAINNET_L2_OWNER_ADDRESS,
       treasuryAddress: process.env.BASE_MAINNET_TREASURY_ADDRESS,
@@ -228,6 +231,10 @@ const config: HardhatUserConfig = {
         process.env.BASE_MAINNET_WORKER_HUB_SCORING_ADDRESS,
       hybridModelAddress_1: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_1,
       hybridModelAddress_2: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_2,
+      hybridModelAddress_3: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_3,
+      hybridModelAddress_4: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_4,
+      hybridModelAddress_5: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_5,
+      hybridModelAddress_6: process.env.BASE_MAINNET_HYBRID_MODEL_ADDRESS_6,
       hybridModelScoringAddress:
         process.env.BASE_MAINNET_HYBRID_MODEL_SCORING_ADDRESS,
       systemPromptManagerAddress:
