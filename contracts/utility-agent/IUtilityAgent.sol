555a07b4b8379d4686131e0773d3aad43ebe9d79
kelvin6868 <kelvin@bvm.network>
2025-02-26 16:31:41 +0700

    Update UtilityAgentUpgradeable contract

--- contracts/utility-agent/IUtilityAgent.sol
+++ contracts/utility-agent/IUtilityAgent.sol
@@ -4,111 +4,70 @@ pragma solidity ^0.8.0;
 import {File} from "./IFileStore.sol";
 
 interface IUtilityAgent {
-    struct StorageInfo {
-        address contractAddress;
-        string filename;
+    enum FileType {
+        LIBRARY,
+        DEV_SCRIPT
     }
 
-    event StorageInfoUpdate(uint256 indexed cfIndex, StorageInfo newInfo);
-    event StorageInfoRemoved(uint256 indexed cfIndex);
+    struct AgentLogicConfig {
+        address fileStore;
+        FileType fileType;
+        string fileName;
+    }
+
+    event AgentLogicConfigCreate(
+        uint256 indexed cfIndex,
+        AgentLogicConfig newConfig
+    );
+    event AgentLogicConfigUpdate(
+        uint256 indexed cfIndex,
+        AgentLogicConfig newConfig
+    );
+    event AgentLogicConfigRemove(uint256 indexed cfIndex);
     event EndpointUpdate(string key, string value);
-    event SystemPromptUpdate(string newSystemPrompt);
 
     error InvalidData();
     error ZeroAddress();
 
-    /**
-     * @notice Updates the storage information for a given index.
-     * @param cfIndex The index of the storage information to update.
-     * @param storageInfo The new storage information.
-     */
-    function updateStorageInfo(
-        uint256 cfIndex,
-        StorageInfo calldata storageInfo
+    function addNewAgentConfig(
+        AgentLogicConfig[] calldata logicCfs,
+        string[] calldata keys,
+        string[] calldata values
     ) external;
 
-    /**
-     * @notice Fetches all file codes.
-     * @return filename An array of filenames.
-     * @return logic An array of file contents.
-     */
-    function fetchAllFileCodes()
-        external
-        view
-        returns (string[] memory filename, string[] memory logic);
+    function updateAgentLogicConfig(
+        uint256 cfIdx,
+        AgentLogicConfig calldata cf
+    ) external;
 
-    /**
-     * @notice Fetches the file code from a given storage configuration.
-     * @param config The storage configuration.
-     * @return logic The file content.
-     */
-    function fetchFileCodeFromConfig(
-        StorageInfo memory config
-    ) external view returns (string memory logic);
+    function removeAgentLogicConfig(uint256 cfIdx) external;
 
-    /**
-     * @notice Gets the storage mode for a given storage configuration.
-     * @param config The storage configuration.
-     * @return The storage mode ("fs" or "ipfs").
-     */
-    function getStorageMode(
-        StorageInfo memory config
-    ) external view returns (string memory);
-
-    /**
-     * @notice Gets all storage configurations.
-     * @return configs An array of storage configurations.
-     */
-    function getStorageInfo()
-        external
-        view
-        returns (StorageInfo[] memory configs);
-
-    /**
-     * @notice Updates the system prompt.
-     * @param _systemPrompt The new system prompt.
-     */
-    function updateSystemPrompt(string calldata _systemPrompt) external;
-
-    /**
-     * @notice Gets the system prompt.
-     * @return The system prompt.
-     */
-    function getSystemPrompt() external view returns (string memory);
-
-    /**
-     * @notice Updates multiple endpoints.
-     * @param keys An array of endpoint keys.
-     * @param values An array of endpoint values.
-     */
     function updateEndpoints(
         string[] calldata keys,
         string[] calldata values
     ) external;
 
-    /**
-     * @notice Updates a single endpoint.
-     * @param key The endpoint key.
-     * @param value The endpoint value.
-     */
-    function updateEndpoint(
-        string calldata key,
-        string calldata value
-    ) external;
+    function getEndpoints(
+        string[] calldata keys
+    ) external view returns (string[] memory values);
 
-    /**
-     * @notice Gets the value of an endpoint.
-     * @param key The endpoint key.
-     * @return value The endpoint value.
-     */
-    function getEndpoint(
-        string calldata key
-    ) external view returns (string memory value);
+    function fetchAllAgentLogic()
+        external
+        view
+        returns (string[] memory filename, string[] memory logic);
 
-    /**
-     * @notice Returns the number of configurations.
-     * @dev This function is a view function, meaning it does not modify the state.
-     * @return The number of configurations as a uint256.
-     */
+    function fetchLogicByConfig(
+        AgentLogicConfig memory cf
+    ) external view returns (string memory logic);
+
+    function getStorageMode(
+        AgentLogicConfig memory config
+    ) external view returns (string memory);
+
+    function getAgentLogicConfigs()
+        external
+        view
+        returns (AgentLogicConfig[] memory configs);
+
     function getConfigsNumber() external view returns (uint256);
 }
--- contracts/utility-agent/UtilityAgentUpgradeable.sol
+++ contracts/utility-agent/UtilityAgentUpgradeable.sol
@@ -8,131 +8,164 @@ import {IFileStore, File} from "./IFileStore.sol";
 contract UtilityAgentUpgradeable is IUtilityAgent, OwnableUpgradeable {
     bytes32 private constant _IPFS_SIG = keccak256(bytes("ipfs"));
 
-    string private _systemPrompt;
+    string public builtWith = "javascript"; // e.g., "python", "java"
     mapping(string => string) private _endPoints;
     uint256 private _configsNum;
-    mapping(uint256 => StorageInfo) private _storageInfos;
+    mapping(uint256 => AgentLogicConfig) private _agentLogicConfigs;
 
     uint256[50] private __gap;
 
-    /**
-     * @notice Initializes the contract.
-     * @param systemPrompt_ The initial system prompt.
-     * @param storageInfos_ The initial storage information.
-     */
     function initialize(
-        string memory systemPrompt_,
-        StorageInfo[] calldata storageInfos_
+        AgentLogicConfig[] calldata logicCfs
     ) external initializer {
         __Ownable_init();
-        _systemPrompt = systemPrompt_;
 
-        uint256 len = storageInfos_.length;
+        uint256 len = logicCfs.length;
+
         for (uint256 i = 0; i < len; i++) {
-            _addNewStorageInfo(storageInfos_[i]);
+            _addNewAgentLogicConfig(logicCfs[i]);
         }
     }
 
-    function addNewStorageInfo(
-        StorageInfo calldata storageInfo
+    function addNewAgentConfig(
+        AgentLogicConfig[] calldata logicCfs,
+        string[] calldata keys,
+        string[] calldata values
     ) external virtual onlyOwner {
-        _addNewStorageInfo(storageInfo);
+        uint256 epLen = keys.length;
+
+        if (epLen != values.length) {
+            revert InvalidData();
+        }
+
+        uint256 cfLen = logicCfs.length;
+
+        for (uint256 i = 0; i < cfLen; i++) {
+            _addNewAgentLogicConfig(logicCfs[i]);
+        }
+
+        for (uint256 i = 0; i < epLen; i++) {
+            _updateEndpoint(keys[i], values[i]);
+        }
     }
 
     /**
      * @notice Adds new storage information.
      * @param storageInfo The storage information to add.
      */
-    function _addNewStorageInfo(
-        StorageInfo calldata storageInfo
+    function _addNewAgentLogicConfig(
+        AgentLogicConfig calldata storageInfo
     ) internal virtual {
-        _storageInfos[_configsNum] = storageInfo;
-        emit StorageInfoUpdate(_configsNum, storageInfo);
+        _agentLogicConfigs[_configsNum] = storageInfo;
+        emit AgentLogicConfigCreate(_configsNum, storageInfo);
         _configsNum++;
     }
 
-    /**
-     * @notice Updates the storage information for a given index.
-     * @param cfIndex The index of the storage information to update.
-     * @param storageInfo The new storage information.
-     */
-    function updateStorageInfo(
-        uint256 cfIndex,
-        StorageInfo calldata storageInfo
+    function updateAgentLogicConfig(
+        uint256 cfIdx,
+        AgentLogicConfig calldata cf
     ) external onlyOwner {
-        if (cfIndex >= _configsNum) {
+        if (cfIdx >= _configsNum) {
             revert InvalidData();
         }
-        _updateStorageInfo(cfIndex, storageInfo);
+        _updateAgentLogicConfig(cfIdx, cf);
     }
 
-    /**
-     * @notice Updates the storage information for a given index.
-     * @param cfIndex The index of the storage information to update.
-     * @param storageInfo The new storage information.
-     */
-    function _updateStorageInfo(
-        uint256 cfIndex,
-        StorageInfo calldata storageInfo
+    function _updateAgentLogicConfig(
+        uint256 cfIdx,
+        AgentLogicConfig calldata cf
     ) internal virtual {
-        _storageInfos[cfIndex] = storageInfo;
-        emit StorageInfoUpdate(cfIndex, storageInfo);
+        _agentLogicConfigs[cfIdx] = cf;
+        emit AgentLogicConfigUpdate(cfIdx, cf);
     }
 
-    /**
-     * @notice Removes the storage information for a given index.
-     * @param cfIndex The index of the storage information to remove.
-     */
-    function removeStorageInfo(uint256 cfIndex) external onlyOwner {
-        if (cfIndex >= _configsNum) {
+    function removeAgentLogicConfig(uint256 cfIdx) external onlyOwner {
+        if (cfIdx >= _configsNum) {
             revert InvalidData();
         }
 
         uint256 lastIdx = _configsNum - 1;
-        if (cfIndex < lastIdx) {
-            _storageInfos[cfIndex] = _storageInfos[lastIdx];
+        if (cfIdx < lastIdx) {
+            _agentLogicConfigs[cfIdx] = _agentLogicConfigs[lastIdx];
         }
 
-        delete _storageInfos[lastIdx];
+        delete _agentLogicConfigs[lastIdx];
         _configsNum--;
-        emit StorageInfoRemoved(cfIndex);
+        emit AgentLogicConfigRemove(cfIdx);
     }
 
-    /**
-     * @notice Updates the system prompt.
-     * @param systemPrompt The new system prompt.
-     */
-    function updateSystemPrompt(
-        string calldata systemPrompt
+    function updateEndpoints(
+        string[] calldata keys,
+        string[] calldata values
     ) external onlyOwner {
-        _updateSystemPrompt(systemPrompt);
+        uint256 len = keys.length;
+
+        if (len != values.length) {
+            revert InvalidData();
+        }
+
+        for (uint256 i = 0; i < len; i++) {
+            _updateEndpoint(keys[i], values[i]);
+        }
     }
 
-    /**
-     * @notice Updates the system prompt.
-     * @param systemPrompt The new system prompt.
-     */
-    function _updateSystemPrompt(
-        string calldata systemPrompt
+    function _updateEndpoint(
+        string calldata key,
+        string calldata value
     ) internal virtual {
-        _systemPrompt = systemPrompt;
-        emit SystemPromptUpdate(systemPrompt);
+        _endPoints[key] = value;
+        emit EndpointUpdate(key, value);
     }
 
-    /**
-     * @notice Gets the system prompt.
-     * @return The system prompt.
-     */
-    function getSystemPrompt() external view returns (string memory) {
-        return _systemPrompt;
+    function getEndpoints(
+        string[] calldata keys
+    ) external view returns (string[] memory values) {
+        uint256 len = keys.length;
+        values = new string[](len);
+
+        for (uint256 i = 0; i < len; i++) {
+            values[i] = _endPoints[keys[i]];
+        }
     }
 
-    /**
-     * @notice Fetches all file codes.
-     * @return filename An array of filenames.
-     * @return logic An array of file contents.
-     */
-    function fetchAllFileCodes()
+    function fetchCode() external view returns (string memory code) {
+        string memory libsCode = "";
+        string memory devScripts = "";
+        uint256 len = _configsNum;
+        // logic = new string[](len);
+
+        for (uint256 i = 0; i < len; i++) {
+            AgentLogicConfig memory cf = _agentLogicConfigs[i];
+            string memory trunk = fetchLogicByConfig(cf);
+
+            if (cf.fileType == FileType.LIBRARY) {
+                libsCode = string(abi.encodePacked(libsCode, trunk));
+            } else if (cf.fileType == FileType.DEV_SCRIPT) {
+                devScripts = string(abi.encodePacked(devScripts, trunk));
+            }
+        }
+
+        return _buildScript(libsCode, devScripts);
+    }
+
+    function _buildScript(
+        string memory libsCode,
+        string memory devScripts
+    ) internal view returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    '<script sandbox="allow-scripts" type="text/javascript" name="DECOMPRESS_LIB" src="data:@file/javascript;base64,',
+                    libsCode,
+                    '"></script>',
+                    '<script name="dev">getGzipFile(dataURItoBlob("',
+                    devScripts,
+                    '"));</script>'
+                )
+            );
+    }
+
+    function fetchAllAgentLogic()
         external
         view
         virtual
@@ -141,105 +174,46 @@ contract UtilityAgentUpgradeable is IUtilityAgent, OwnableUpgradeable {
         uint256 len = _configsNum;
         filename = new string[](len);
         logic = new string[](len);
+
         for (uint256 i = 0; i < len; i++) {
-            filename[i] = _storageInfos[i].filename;
-            logic[i] = fetchFileCodeFromConfig(_storageInfos[i]);
+            filename[i] = _agentLogicConfigs[i].fileName;
+            logic[i] = fetchLogicByConfig(_agentLogicConfigs[i]);
         }
     }
 
-    /**
-     * @notice Fetches the file code from a given storage configuration.
-     * @param config The storage configuration.
-     * @return logic The file content.
-     */
-    function fetchFileCodeFromConfig(
-        StorageInfo memory config
+    function fetchLogicByConfig(
+        AgentLogicConfig memory cf
     ) public view virtual returns (string memory logic) {
-        if (keccak256(bytes(getStorageMode(config))) == _IPFS_SIG) {
-            logic = config.filename; // return the IPFS hash
+        if (keccak256(bytes(getStorageMode(cf))) == _IPFS_SIG) {
+            logic = cf.fileName; // return the IPFS hash
         } else {
-            logic = IFileStore(config.contractAddress)
-                .getFile(config.filename)
-                .read();
+            logic = IFileStore(cf.fileStore).getFile(cf.fileName).read();
         }
     }
 
-    /**
-     * @notice Gets the storage mode for a given storage configuration.
-     * @param config The storage configuration.
-     * @return The storage mode ("fs" or "ipfs").
-     */
     function getStorageMode(
-        StorageInfo memory config
+        AgentLogicConfig memory cf
     ) public view virtual returns (string memory) {
-        if (config.contractAddress != address(0)) {
+        if (cf.fileStore != address(0)) {
             return "fs";
         } else {
             return "ipfs";
         }
     }
 
-    /**
-     * @notice Gets all storage configurations.
-     * @return configs An array of storage configurations.
-     */
-    function getStorageInfo()
+    function getAgentLogicConfigs()
         external
         view
-        returns (StorageInfo[] memory configs)
+        returns (AgentLogicConfig[] memory configs)
     {
         uint256 len = _configsNum;
-        configs = new StorageInfo[](len);
+        configs = new AgentLogicConfig[](len);
 
         for (uint256 i = 0; i < len; i++) {
-            configs[i] = _storageInfos[i];
+            configs[i] = _agentLogicConfigs[i];
         }
     }
 
-    /**
-     * @notice Updates multiple endpoints.
-     * @param keys An array of endpoint keys.
-     * @param values An array of endpoint values.
-     */
-    function updateEndpoints(
-        string[] calldata keys,
-        string[] calldata values
-    ) external onlyOwner {
-        uint256 len = keys.length;
-        for (uint256 i = 0; i < len; i++) {
-            updateEndpoint(keys[i], values[i]);
-        }
-    }
-
-    /**
-     * @notice Updates a single endpoint.
-     * @param key The endpoint key.
-     * @param value The endpoint value.
-     */
-    function updateEndpoint(
-        string calldata key,
-        string calldata value
-    ) public virtual onlyOwner {
-        _endPoints[key] = value;
-        emit EndpointUpdate(key, value);
-    }
-
-    /**
-     * @notice Gets the value of an endpoint.
-     * @param key The endpoint key.
-     * @return value The endpoint value.
-     */
-    function getEndpoint(
-        string calldata key
-    ) external view returns (string memory value) {
-        value = _endPoints[key];
-    }
-
-    /**
-     * @notice Returns the number of configurations.
-     * @dev This function is a view function, meaning it does not modify the state.
-     * @return The number of configurations as a uint256.
-     */
     function getConfigsNumber() external view returns (uint256) {
         return _configsNum;
     }
